a967c40590ea18c720c10e117650b99eac13a19c
SvenGustav <111660594+SvenGustav@users.noreply.github.com>
2023-06-21 15:00:23 +0300

    Update streamlit_app.py
    Updated Meny

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title('New Healthy Diner')
 
-streamlit.header('Breakfast Meny')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Green Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Green Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
